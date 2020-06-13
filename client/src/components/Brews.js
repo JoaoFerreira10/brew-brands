5aeff63a7048e7135c6bf2fa3d8f38d71a06070f
Joao Ferreira <joao.ferreira@mindera.com>
2020-06-14 00:09:22 +0100

    Added cart

--- client/src/components/Brews.js
+++ client/src/components/Brews.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import {Box, Text, Heading, Card, Image, Button} from 'gestalt';
+import {Box, Text, Heading, Card, Image, Button, Mask, IconButton} from 'gestalt';
 import Strapi from 'strapi-sdk-javascript/build/main';
+import { setCart, getCart } from "../utils";
 
 const apiUrl = process.env.API_URL || 'http://localhost:1337';
 const strapi = new Strapi(apiUrl);
 
 class Brews extends Component {
 
-    state = {brews: [], brand: ''};
+    state = {brews: [], brand: '', cartItems: []};
 
     async componentDidMount() {
 
@@ -32,18 +33,57 @@ class Brews extends Component {
                 }
               }
            );
-console.log("FSD", data)
-           this.setState({brews: data.brand.brews, brand: data.brand.name});
+
+           this.setState({
+               brews: data.brand.brews, 
+               brand: data.brand.name,
+                cartItems: getCart() || []
+            });
 
         } catch (err) {
             console.error(err);
         }
     }
 
+    
+    handleAddToCart = brew => {
+
+        const { cartItems } = this.state;
+
+        const alreadyInCart = cartItems.findIndex(item => item._id === brew._id);
+
+        if(alreadyInCart === -1) {
+            const updateCart = cartItems.concat({...brew, quantity: 1});
+
+            this.setState({cartItems: updateCart}, () => setCart(updateCart));
+        }
+        else {
+
+            const updateCart = [...cartItems];
+            updateCart[alreadyInCart].quantity += 1;
+            this.setState({cartItems: updateCart}, () => setCart(updateCart));
+        }
+
+    };
+
+    handleDeleteFromCart = itemId => {
+
+        const { cartItems } = this.state;
+
+        const items = cartItems.filter(item => item._id !== itemId);
+
+        this.setState({cartItems: items}, ()=> setCart(items))
+    }
+
     render() {
-        const {brand, brews} = this.state;
+        const {brand, brews, cartItems} = this.state;
         return(
-            <Box marginTop={4} display="flex" justifyContent="center" alignItems="start">
+            <Box marginTop={4} display="flex" justifyContent="center" alignItems="start" 
+            dangerouslySetInlineStyle={{
+                __style: {
+                    flexWrap: 'wrap-reverse'
+                }
+            }}>
                 <Box display="flex" direction="column" alignItems="center">
                     <Box margin={2}>
                         <Heading color="orchid">{brand}</Heading>
@@ -51,36 +91,64 @@ console.log("FSD", data)
                     <Box
                     wrap
                     shape="rounded" display="flex" justifyContent="center">
-          {
-            brews.map(brew =>(
-              <Box paddingY={4} margin={2} width={220} key={brew._id}> 
-                <Card>
-                  
-                    <Box height={260} width={200} paddingY={2}>
-                      <Image
-                      alt="Brew"
-                      naturalHeight={1}
-                      naturalWidth={1}
-                      src={`${apiUrl}${brew.image[0] && brew.image[0].url}`}
-                      />
+                    {
+                        brews.map(brew =>(
+                        <Box paddingY={4} margin={2} width={220} key={brew._id}> 
+                            <Card>
+                            
+                                <Box height={260} width={200} paddingY={2}>
+                                <Image
+                                alt="Brew"
+                                naturalHeight={1}
+                                naturalWidth={1}
+                                src={`${apiUrl}${brew.image[0] && brew.image[0].url}`}
+                                />
+                                </Box>
+                            
+                            </Card> 
+                            <Box marginBottom={2}>
+                                <Text bold size="xl">{brew.name}</Text>
+                            </Box>
+                            <Text>{brew.description}</Text>
+                            <Text color="orchid">{brew.price}€</Text>
+                            <Box marginTop={2}>
+                                <Text size="lg">
+                                <Button color="blue" text="Add to cart" onClick={()=> this.handleAddToCart(brew)}></Button>
+                                </Text>
+                            </Box>
+                        </Box>
+                        ))
+                    }
                     </Box>
-                  
-                </Card> 
-                <Box marginBottom={2}>
-                    <Text bold size="xl">{brew.name}</Text>
                 </Box>
-                <Text>{brew.description}</Text>
-                <Text color="orchid">{brew.price}€</Text>
-                <Box marginTop={2}>
-                    <Text size="lg">
-                    <Button color="blue" text="Add to cart"></Button>
-                    </Text>
-                </Box>
-              </Box>
-            ))
-          }
 
-                    </Box>
+                {/* Cart */}
+                <Box alignSelf="end" marginTop={2} marginLeft={12}>
+                    <Mask shape="rounded" wash>
+                        <Box
+                        display="flex"
+                        direction="column"
+                        alignItems="center"
+                        padding={2}>
+                            <Heading align="center" size="md">
+                                My Cart
+                            </Heading>
+                            <Text color="gray" italic>{cartItems.length} items selected</Text>
+                            {cartItems.map(item=>(
+                                <Box key={item._id} display="flex" alignItems="center">
+                                    <Text>
+                                        {item.name} x {item.quantity} - {(item.quantity * item.price).toFixed(2)}€
+                                    </Text>
+                                    <IconButton
+                                    icon="cancel"
+                                    accessibilityLabel="Delete"
+                                    size="sm"
+                                    iconColor="red"
+                                    onClick={() => this.handleDeleteFromCart(item._id)}></IconButton>
+                                </Box>
+                            ))}
+                        </Box>
+                    </Mask>
                 </Box>
             </Box>
         );
